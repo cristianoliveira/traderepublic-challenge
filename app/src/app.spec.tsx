ef6352c88ed5b7ed0393b945b00006d53c2ccfde
cristianoliveira <contato@cristianoliveira.com.br>
2024-07-24 19:26:00 +0200

    chore: adds some example tests

--- app/src/app.spec.tsx
+++ app/src/app.spec.tsx
@@ -1,14 +1,18 @@
 import { describe, expect, it } from "vitest";
 
 import { render, screen } from "@testing-library/preact";
-// import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 
 import { App } from "./app.tsx";
 
 describe("App", () => {
-  it("renders", () => {
+  it("renders", async () => {
     render(<App />);
     expect(screen.getByText("count is 0")).toBeInTheDocument();
+
+    await userEvent.click(screen.getByText("count is 0"));
+
+    expect(screen.getByText("count is 1")).toBeInTheDocument();
   });
 });
 
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+// @ts-ignore vite-plugin-eslint doesn't export types
 import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
